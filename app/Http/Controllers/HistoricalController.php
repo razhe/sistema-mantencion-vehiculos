2f59eecebee0425c3e8b6880daa61fa839459022
razhe <gustavo.acu.rivas@gmail.com>
2023-01-24 12:00:07 -0300

    Creacion de los modelos y los controladores.

---
+++ app/Http/Controllers/HistoricalController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Historical;
+use Illuminate\Http\Request;
+
+class HistoricalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Historical  $historical
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Historical $historical)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Historical  $historical
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Historical $historical)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Historical  $historical
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Historical $historical)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Historical  $historical
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Historical $historical)
+    {
+        //
+    }
+}
